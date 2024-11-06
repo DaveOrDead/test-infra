12633ecb3654cb87e8ecb48641d7c00a621babeb
Dave Berner <davidajberner@gmail.com>
2024-11-06 19:48:01 +1100

    feat: update structure

--- kindeSrc/environment/workflows/daveTestWorkflow.ts
+++ kindeSrc/environment/workflows/daveTestWorkflow.ts
@@ -17,11 +17,13 @@ export const workflowSettings = {
   },
 };
 
-export default async function canBeAnyName({ context, request }) {
-  const baseURL = context.domains.kindeDomain;
-  const kindeAPI = await createKindeAPI(baseURL);
-  const users = await kindeAPI.get("users");
-  console.log({ users });
+export default {
+  async canBeAnyName({ context, request }) {
+    const baseURL = context.domains.kindeDomain;
+    const kindeAPI = await createKindeAPI(baseURL);
+    const users = await kindeAPI.get("users");
+    console.log({ users });
 
-  return "testing return";
-}
+    return "testing return";
+  },
+};
