04a7b2ec695829805a21ffaf68441540052b6608
Dave Berner <davidajberner@gmail.com>
2024-11-06 19:37:16 +1100

    feat: add WF settings

--- kindeSrc/environment/workflows/daveTestWorkflow.ts
+++ kindeSrc/environment/workflows/daveTestWorkflow.ts
@@ -1,5 +1,21 @@
 import { createKindeAPI } from "./utils/getKindeAPI";
 
+export const workflowSettings = {
+  id: "addUserTokenClaim",
+  name: "User token generation",
+  trigger: "user:tokens_generation",
+  bindings: {
+    console: {},
+    "kinde.fetch": {},
+    "kinde.idToken": {
+      resetClaims: true,
+    },
+    "kinde.accessToken": {
+      resetClaims: true,
+    },
+  },
+};
+
 export default async function CanBeAnyName({ context, request }) {
   const baseURL = context.domains.kindeDomain;
   const kindeAPI = await createKindeAPI(baseURL);
