50978a298265fde12e87ff401bd5407835cc6cd1
Dave Berner <davidajberner@gmail.com>
2024-11-06 19:55:11 +1100

    feat: test rename default export

--- kindeSrc/environment/workflows/userTokens/daveTestWorkflow.ts
+++ kindeSrc/environment/workflows/userTokens/daveTestWorkflow.ts
@@ -17,13 +17,11 @@ export const workflowSettings = {
   },
 };
 
-export default {
-  async canBeAnyName({ context, request }) {
-    const baseURL = context.domains.kindeDomain;
-    const kindeAPI = await createKindeAPI(baseURL);
-    const users = await kindeAPI.get("users");
-    console.log({ users });
+export default async function canBeAnyName({ context, request }) {
+  const baseURL = context.domains.kindeDomain;
+  const kindeAPI = await createKindeAPI(baseURL);
+  const users = await kindeAPI.get("users");
+  console.log({ users });
 
-    return "testing return";
-  },
-};
+  return "testing return";
+}
